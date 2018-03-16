22e0dbb5d1909df73d8c6918fcab94f9486b0bd4
Unknown <xxshuyinxx@gmail.com>
2018-03-16 16:40:58 +0100

    Login Form
    Oppdatert!

--- js/login.js
+++ js/login.js
@@ -1,43 +1,13 @@
-$('.form').find('input, textarea').on('keyup blur focus', function (e) {
-  
-  var $this = $(this),
-      label = $this.prev('label');
+jQuery(document).ready(function($) {
+  tab = $('.tabs h3 a');
 
-	  if (e.type === 'keyup') {
-			if ($this.val() === '') {
-          label.removeClass('active highlight');
-        } else {
-          label.addClass('active highlight');
-        }
-    } else if (e.type === 'blur') {
-    	if( $this.val() === '' ) {
-    		label.removeClass('active highlight'); 
-			} else {
-		    label.removeClass('highlight');   
-			}   
-    } else if (e.type === 'focus') {
-      
-      if( $this.val() === '' ) {
-    		label.removeClass('highlight'); 
-			} 
-      else if( $this.val() !== '' ) {
-		    label.addClass('highlight');
-			}
-    }
+  tab.on('click', function(event) {
+    event.preventDefault();
+    tab.removeClass('active');
+    $(this).addClass('active');
 
-});
-
-$('.tab a').on('click', function (e) {
-  
-  e.preventDefault();
-  
-  $(this).parent().addClass('active');
-  $(this).parent().siblings().removeClass('active');
-  
-  target = $(this).attr('href');
-
-  $('.tab-content > div').not(target).hide();
-  
-  $(target).fadeIn(600);
-  
+    tab_content = $(this).attr('href');
+    $('div[id$="tab-content"]').removeClass('active');
+    $(tab_content).addClass('active');
+  });
 });
