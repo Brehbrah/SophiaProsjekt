fdc568cca3e02f894d66d723928c017adaf9d34e
Vidar Årvik <vidaraarvik@gmail.com>
2018-03-25 20:25:45 +0200

    endret treningsdagbok

---
+++ js/dashboard.js
@@ -0,0 +1,19 @@
+function endreØvelse(valg) {
+	var øvelse = valg.value;
+
+	switch(øvelse) {
+    case "Løping":
+        document.getElementById("antLabel").innerHTML = "Antall km løpt";
+        break;
+    case "Svømming":
+        document.getElementById("antLabel").innerHTML = "Antall km svømt";
+        break;
+   	case "Sykling":
+   		document.getElementById("antLabel").innerHTML = "Antall km syklet";
+        break;
+   	case "Vektløfting":
+   		document.getElementById("antLabel").innerHTML = "Antall øvelser";
+        break;
+	}
+	
+}
