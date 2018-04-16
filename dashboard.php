2c4368c9a7487482e3a2eeb99437a6c2691782f8
Unknown <xxshuyinxx@gmail.com>
2018-04-16 16:21:21 +0200

    Bootstraps - Oppdatering
    Dashboard - navmeny

--- dashboard.php
+++ dashboard.php
@@ -1,3 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+  <title>Bootstrap</title>
+</head>
+  <body>
+    <nav class="navbar bg-dark navbar-dark navbar-expand-sm">
+      <div class ="container">
+        <a class ="navbar-brand d-none d-sm-inline-block"> 
+           <a class="navbar-brand" href="dashboard.php">Sophie Prosjekt</a>
+        </a>
+            <a class ="nav-item nav link" href=loggut.php>Logg ut</a>
+        </a>
+      </div>
+    </nav>
+  
+  <div class="container">
+
 <?php
 session_start();
 include_once "funksjoner.php";
@@ -9,12 +32,6 @@ $bruker = $_SESSION['brukernavn'];
 topp();
 ?>
 
-	<div class='topnav'>
-		<a href='loggut.php'>Logg ut</a>
-		<a href='Contact.php'>Kontakt</a>
-		<a href='About.php'>Om oss</a>
-	</div>
-
     <section class="jumbotron text-center">
       <div class="container-fluid">
         <h1 class="jumbotron-heading">Hei, <?php echo "$bruker"; ?>!</h1>
@@ -22,7 +39,6 @@ topp();
         <hr class="my-4">
         <p class="lead text-muted">Velkommen til ditt dashboard, hva vil du gjøre nå?</p>
         <p>
-          <a href="dashboard.php" class="btn btn-primary my-2">Dashboard</a>
           <a href="treningsdagbok.php" class="btn btn-secondary my-2">Treningsdagbok</a>
           <a href="preferanser.php" class="btn btn-secondary my-2">Preferanser</a>
           <a href="treningsplan.php" class="btn btn-secondary my-2">Treningsplan</a>
@@ -58,3 +74,11 @@ topp();
 	
 	
 <?php bunn(); ?>
+
+  </div><!-- content container -->
+
+  <script src="js/jquery.slim.min.js"></script>
+  <script src="js/popper.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
