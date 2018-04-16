ce44a7b946226c5ef0573088574bfd672520ae11
Tom <kizyup@live.no>
2018-04-16 14:32:25 +0200

    Kontakt form

---
+++ config/config.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'subject' => [
+        'prefix' => '[Contact Form]'
+    ],
+    'emails' => [
+        'to'   => 'kizyup@live.no',
+        'from' => ''
+    ],
+    'messages' => [
+        'error'   => 'Noe gikk galt. Prøv igjen nå eller senere.',
+        'success' => 'Meldingen din har blir sendt.'
+    ],
+    'fields' => [
+        'name'     => 'Navn',
+        'email'    => 'Epost',
+        'phone'    => 'Telefon',
+        'subject'  => 'Tittel',
+        'message'  => 'Melding',
+        'btn-send' => 'Send'
+    ]
+];
