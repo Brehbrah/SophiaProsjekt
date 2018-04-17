f78bd215182d937803e3ee3b6c095e8360c1049b
Unknown <Kasim300@hotmail.com>
2018-04-17 13:42:47 +0200

    Endret kontakt siden og la in spøreskjemaet
    Endret kontakt siden og la in spøreskjemaet

--- Contact.php
+++ Contact.php
@@ -23,9 +23,19 @@ topp();
 	<main>
 	<br>
 		<h1>Contact Form</h1>
-		
 		<div class="container" >
-		
+		<p>Har du noen spørsmål, eller diskusjons tema du vi ta opp, så legg inn en e-post og send den til meg. Jeg skal prøve å svare på den så fort som mulig. </p>
+		<div class="form">
+			<form action="mailto:support@sophia.no" method="post" enctype="text/plain">
+			Navn<br>
+			<input type="text" name="name"><br>
+			E-post:<br>
+			<input type="text" name="mail"><br>
+			Kommentar:<br>
+			<input type="text" style="height:150px" name="comment" size="50"><br><br>
+			<input type="submit" value="Send">
+			</form>
+	</div>
 		
 		
 
